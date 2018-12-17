6a55f14411286fe6be177d7c33000d747f076a34
zhoujian <zhoujian@newtank.cn>
2018-12-17 14:51:42 +0800

    t-job

---
+++ t-core/src/main/java/com/t/logger/JobLoggerFactory.java
@@ -0,0 +1,13 @@
+package com.t.logger;
+
+/**
+ * @author tb
+ * @date 2018/12/17 14:48
+ */
+public class JobLoggerFactory {
+
+    public JobLogger getJobLogger() {
+
+        return null;
+    }
+}
